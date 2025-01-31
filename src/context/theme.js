18bd0ce28c80d1885fb6684fb367ca2d06d91662
Rachel Kirkland <100070583+littleangelx@users.noreply.github.com>
2025-01-31 17:30:21 +0000

    try to fix localstorage issue

--- src/context/theme.js
+++ src/context/theme.js
@@ -5,24 +5,11 @@ import React, { createContext, useState, useEffect, useContext } from "react";
 // Create a context with 'light' as the default value
 const ThemeContext = createContext();
 
-const getLocalStorageValue = (key) => {
-  if (typeof window !== "undefined") {
-    return localStorage.getItem(key);
-  }
-  // Fallback for server-side
-  return null;
-};
-
 export const ThemeProvider = ({ children }) => {
   const getInitialTheme = () => {
-    const savedTheme = getLocalStorageValue("theme");
-    if (savedTheme) {
-      return savedTheme;
-    } else {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
-    }
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
   };
 
   const [theme, setTheme] = useState(getInitialTheme);
